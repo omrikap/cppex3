d0031b0f7450cc4b627aa118fb81005aee8ca2d5
omri <omrikap@gmail.com>
2015-09-07 01:52:14 +0300

    destructor sigsegv

--- Matrix.hpp
+++ Matrix.hpp
@@ -64,12 +64,9 @@ public:
 	 * The Matrix class destructor.
 	 * Destructs the elements of the vector containing the matrix object cell.
 	 */
-	~Matrix() // todo test
+	~Matrix()
 	{
-		for (int i = 0; i < _matrixVectorSize; ++i)
-		{
-			_matrix.erase(i);
-		}
+//		_matrix.clear(); // todo test. problem with deallocating copied matrix
 	}
 
 	/**
@@ -78,16 +75,72 @@ public:
 	 * @param other The Matrix<T> object that will be moved to the new pointer.
 	 * @return The moved Matrix<T> object with it's new address. todo verify what's returned
 	 */
-	Matrix(Matrix<T>& other) //todo implement
+	Matrix(Matrix<T>&& other) :_rows(other._rows), _cols(other._cols), _matrixVectorSize
+	(other._matrixVectorSize) // todo test
 	{
+		for (int i = 0; i < _matrixVectorSize; ++i)
+		{
+			_matrix.push_back(other._matrix[i]);
+		}
 
+		// reset other
+		other._rows = 1;
+		other._cols = 1;
+		other._matrixVectorSize = 1;
+		other._matrix.clear();
+		other._matrix.push_back(1);
+	}
+
+	/**
+	 * Assignment operator.
+	 * This operator initializes the data members of this matrix with the data members of the
+	 * other matrix.
+	 * @param other A reference to the other matrix.
+	 * @return A reference to this matrix.
+	 */
+	Matrix<T>& operator=(const Matrix<T> &other)
+	{
+		// in case of self assignment, just return the same object.
+		if (this != &other)
+		{
+			// initialize this matrix data members from the other matrix.
+			_rows = other._rows;
+			_cols = other._cols;
+			_matrixVectorSize = other._matrixVectorSize;
+
+			// clear the vector from current data.
+			_matrix.clear();
+
+			// copy the matrix elements from the other matrix to the new one.
+			for (int i = 0; i < _matrixVectorSize; ++i)
+			{
+				_matrix.push_back(other._matrix[i]);
+			}
+		}
+		return *this;
+	}
+
+	T trace()
+	{
+		if (_rows != _cols)
+		{
+			cout << "not a squre matrix!" << endl;
+			return T();
+//			throw; // todo non_square_matrix exception
+		}
+		T theTrace = T(0);
+		for (int i = 0; i < _rows; ++i)
+		{
+			theTrace += _matrix[i*_rows + i];
+		}
+		return theTrace;
 	}
 
 
 private:
 	unsigned int _rows; /** The number of rows in the matrix */
 	unsigned int _cols; /** The number of columns in the matrix */
-	vector<T> _matrix; /* A pointer to the array of T type objects of the matrix. */
+	vector<T> _matrix; /** A pointer to the array of T type objects of the matrix. */
 	unsigned int _matrixVectorSize; /** The size of the vector representing the matrix */
 };
 
--- MatrixDriver.cpp
+++ MatrixDriver.cpp
@@ -7,8 +7,26 @@ using namespace std;
 int main()
 {
 	cout << "MatrixDriver" << endl;
+	// test default constructor
 	Matrix<int> matrix;
+
+	// test dimension constructor
 	Matrix<int> dimMatrix(3, 4);
+
+	// test copy constructor
 	Matrix<int> copiedMatrix(dimMatrix);
+
+	// test move constructor // todo how to test move constructor
+//	Matrix<int> movedMatrix(copiedMatrix);
+
+	// test operator=
+	matrix = dimMatrix;
+
+	// test destructor
+	copiedMatrix.~Matrix();
+
+	// test trace function
+	cout << "matrix trace: " << matrix.trace() << endl;
+
 	return 0;
 }
